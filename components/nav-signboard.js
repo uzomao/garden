5374dead4ec1ca5fa89decfcbdabf5273a275657
uzoma <chidumaga@gmail.com>
2023-03-28 17:03:27 +0100

    adds navigational sign

--- components/nav-signboard.js
+++ components/nav-signboard.js
@@ -3,20 +3,20 @@ import Link from 'next/link'
 
 export default function NavSignboard() {
   return (
-    <ul id={signboardStyles.signboard} class={signboardStyles.signboard}>
-        <li class={signboardStyles.sign}>
+    <ul id={signboardStyles.signboard} className={signboardStyles.signboard}>
+        <li className={signboardStyles.sign}>
             <a href="#enjoyment" id="enjoyment-sign">Enjoyment</a>
         </li>
-        <li class={signboardStyles.sign}>
+        <li className={signboardStyles.sign}>
             <a href="#">Work</a>
         </li>
-        <li class={signboardStyles.sign}>
+        <li className={signboardStyles.sign}>
             <a href="#">Fits</a>
         </li>
-        <li class={signboardStyles.sign}>
+        <li className={signboardStyles.sign}>
             <a href="#">Dispatch</a>
         </li>
-        <li class={signboardStyles.sign}>
+        <li className={signboardStyles.sign}>
             <a href="#">Peeps</a>
         </li>
     </ul>
