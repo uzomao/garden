f0cdbca9a734819716e675a936d8589576c0bedb
uzoma <chidumaga@gmail.com>
2023-03-28 18:36:10 +0100

    works on dream space

--- components/spaces/dream.js
+++ components/spaces/dream.js
@@ -1,14 +1,24 @@
 import Sky from "../elements/sky"
 import Ground from "../elements/ground"
+import Image from "next/image"
+
+import dream from '@/public/images/sign/dream.png'
+
+import styles from '@/styles/Home.module.css'
 
 export default function DreamSpace() {
   return (
     <>
         <Sky>
-            <h1>Dream</h1>
         </Sky>
         <Ground>
-            <h1>Dream</h1>
+            <div className={styles.signcontainer}>
+                <Image 
+                    src={dream}
+                    alt="Stay in the energy of your dreams"
+                    className={styles.dreamsign}
+                />
+            </div>
         </Ground>
     </>
   )
